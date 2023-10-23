8fe80c8eeb5541e4c28a9d2f271385111c8e6414
iaio998 <depalatisiacopo@gmail.com>
2023-10-23 15:23:59 +0200

    Added array with elements

---
+++ javascript/script.js
@@ -0,0 +1,48 @@
+//    <div class="bg-white text-start p-0 rounded-2">
+// <img class="card-img-top rounded-top-2" src="img/angela-caroll-chief-editor.jpg" alt="Title">
+// <div class="card-body text-center py-4">
+//     <h4 class="card-title">A Carrol</h4>
+//     <p class="card-text">
+//         Person
+//     </p>
+// </div>
+// </div>
+
+"use strict";
+
+myAppOurTeam();
+
+function myAppOurTeam() {
+  const teamInfos = [
+    {
+      name: "Wayne Barnett",
+      position: "Founder & CEO",
+      img: "../img/wayne-barnett-founder-ceo.jpg",
+    },
+    {
+      name: "Angela Carrol",
+      position: "Chief Editor",
+      img: "../img/angela-caroll-chief-editor.jpg",
+    },
+    {
+      name: "Walter Gordon",
+      position: "Office Manager",
+      img: "../img/walter-gordon-office-manager.jpg",
+    },
+    {
+      name: "Angela Lopez",
+      position: "Social Media Manager",
+      img: "../img/angela-lopez-social-media-manager.jpg",
+    },
+    {
+      name: "Scott Estrada",
+      position: "Developer",
+      img: "../img/scott-estrada-developer.jpg",
+    },
+    {
+      name: "Barbara Ramos",
+      position: "Graphic Designer",
+      img: ".../img/barbara-ramos-graphic-designer.jpg",
+    },
+  ];
+}
